362ac6c9122f5d94377c713e272104cc90d3e480
Jude Lawson <lawsonjdutton@gmail.com>
2018-06-05 09:41:34 -0600

    Completes user story two

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,5 +1,6 @@
 class GenresController < ApplicationController
   def index
     @genres = Genre.all
+    @new_genre = Genre.new
   end
 end
--- spec/features/admin/index_page_spec.rb
+++ spec/features/admin/index_page_spec.rb
@@ -13,15 +13,29 @@ RSpec.describe 'Index Page (Admin)' do
     MovieGenre.create!(movie_id: @movie1.id, genre_id: @genre1.id)
     MovieGenre.create!(movie_id: @movie1.id, genre_id: @genre2.id)
     MovieGenre.create!(movie_id: @movie2.id, genre_id: @genre3.id)
+
+    @admin = User.create!(username: 'admin', password: 'adminlogin', role: 1)
   end
 
   describe 'An admin visits the genre index page' do
     it 'they should see a list of all genre name in the database' do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
       visit genres_path
 
       expect(page).to have_content(@genre1.name)
       expect(page).to have_content(@genre2.name)
       expect(page).to have_content(@genre3.name)
+    end
+
+    it 'they should see a form to create a new genre' do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
+      visit genres_path
+
+      expect(page).to have_content('Create a New Genre')
+      within('#new_genre') do
+        expect(page).to have_content('Name')
+        expect(page).to have_button('Create Genre')
+      end
     end
   end
 end
